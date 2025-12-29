5a6080cc8eced639ceaca4f96c8a51a79615c5f2
Parul Hardaha <parulhardaha@Mac.lan>
2025-12-29 23:13:21 +0530

    Implemented generate-id and expiree functions

--- app.py
+++ app.py
@@ -10,11 +10,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()  #creates all tables from models.py
+
+#generate unique ID
+def generate_id():
+    while True:
+        pid = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        if not Paste.query.get(pid):
+            return pid
+
+#check if paste is expired or views exceeded
+def is_expired(paste):
+    if paste.expires_at and datetime.utcnow() > paste.expires_at:
+        return True
+    if paste.max_views is not None and paste.current_views >= paste.max_views:
+        return True
+    return False
+
+# home
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
 
 
 
