2227ef19e900d2f3389a24dc70238e9022d45a65
Parul Hardaha <parulhardaha@Mac.lan>
2025-12-30 00:08:13 +0530

    Added  paste creation & view route

--- app.py
+++ app.py
@@ -33,6 +33,67 @@ def is_expired(paste):
 def home():
     return render_template("index.html")
 
+# Create paste (POST)
+@app.route("/paste", methods=["POST"])
+def create_paste():
+    data = request.get_json() if request.is_json else request.form
+
+    content = data.get("content")
+    ttl = data.get("ttl")
+    max_views = data.get("max_views")
+
+    if not content:
+        return jsonify({"error": "Content required"}), 400
+
+    paste_id = generate_id()
+
+    expires_at = None
+    if ttl:
+        try:
+            expires_at = datetime.utcnow() + timedelta(seconds=int(ttl))#adds that time difference to the current UTC time giving the exact expiration time
+        except:
+            return jsonify({"error": "Invalid TTL value"}), 400
+
+    paste = Paste(
+        id=paste_id,
+        content=content,
+        expires_at=expires_at,
+        max_views=int(max_views) if max_views else None
+    )
+
+    db.session.add(paste)
+    db.session.commit()
+
+    #request.host_url-gets the base URL of  server
+    url = request.host_url + "paste/" + paste_id
+
+    #if json then return json
+    if request.is_json:
+        return jsonify({"url": url}), 201
+
+    return render_template("view.html", url=url)
+
+#view paste (GET)
+@app.route("/paste/<paste_id>", methods=["GET"])
+def view_paste(paste_id):
+    paste = Paste.query.get_or_404(paste_id)
+
+    if is_expired(paste):
+        abort(404)
+
+    paste.current_views += 1
+    db.session.commit()
+
+    if request.headers.get("Accept") == "application/json":
+        return jsonify({"content": paste.content})
+
+    return render_template("view.html", content=paste.content)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 
    
